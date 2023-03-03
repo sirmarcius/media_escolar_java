958b64eacaeb07aa6c131151659709faeb96059c
Márcio Gusmão <marciogus@gmail.com>
2023-03-03 17:33:53 -0300

    Update MediaEscolar.java

--- MediaEscolar.java
+++ MediaEscolar.java
@@ -1,58 +1,55 @@
 /*
 * Faça um programa que dê o conteito do aluno conforme sua nota média.
 * 
-* Entrada: 4 notas
+* Entrada: 3 notas
 * Saída: média das notas
 * 
 */
-
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
-public class MediaEscolar {
-    
-    private static final int _4 = 4;
+public class App {
 
-    public static void main(final String[] args) {
-        final Scanner ent = new Scanner(System.in);
-        int nota1, nota2, nota3, nota4;
-        int media;
-                
-            // recebe a 1º nota
-            System.out.println("Digite a 1ª nota");
-            nota1 = ent.nextInt();
-            
-            // recebe a 2º nota
-            System.out.println("Digite a 2ª nota");
-            nota2 = ent.nextInt();
-            
-            // recebe a 3º nota
-            System.out.println("Digite a 3ª nota");
-            nota3 = ent.nextInt();
+    public static void main(String[] args) {
 
-            // recebe a 4º nota
-            System.out.println("Digite a 4ª nota");
-            nota4 = ent.nextInt();
-            
-            // calcula a média
-            media = (nota1 + nota2 + nota3 + nota4) / _4;
-            System.out.println("A média do aluno é: " + media + "\n");
-            
-            // mostra a nota do aluno
-            if( (media >= 7)){
-                System.out.println("Aluno Aprovado!");
-            } else if(media == 6){
-                System.out.println("Aluno em Recuperação!");
-            } else if(media == 6.5){
-                System.out.println("Aluno em Recuperação!");
-            } else if(media == 5.5){
-                System.out.println("Aluno Reprovado!");
-            } else if(media <= 5){
-                System.out.println("Aluno Reprovado!");
-            }
-        }       
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Qual o seu nome? ");
+        final var name = ler.nextLine();
+
+        float nota1 = pedirNota(ler, 1);
+        float nota2 = pedirNota(ler, 2);
+        float nota3 = pedirNota(ler, 3);
+
+        ler.close();
+
+        var media = (nota1 + nota2 + nota3) / 3;
+
+        DecimalFormat formato = new DecimalFormat("0.00");
+        String mediaFormatada = formato.format(media);
+
+        System.out.print("Aluno " + name + " a sua média é: " + mediaFormatada + "\n");
+
+        if (media < 6) {
+            System.out.print("Aluno Reprovado \n");
+        } else if (media >= 6 && media <= 6.9) {
+            System.out.print("Aluno em Recuperação \n");
+        } else {
+            System.out.print("Aluno Aprovado \n");
+        }
     }
 
-
-
+    private static float pedirNota(Scanner ler, int n) {
+        float nota;
+        do {
+            System.out.print("Qual a nota " + n + "? ");
+            nota = ler.nextFloat();
+            if (nota < 0 || nota > 10) {
+                System.out.println("Nota inválida. Insira uma nota entre 0 e 10.");
+            }
+        } while (nota < 0 || nota > 10);
+        return nota;
+    }
+}
 
 
